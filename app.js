915a0c1d344929e2d51fff02478ca9b4a098b93f
luuxe <luxelily@gmail.com>
2022-06-24 11:16:02 -0500

    tweaked some dark/light mode styling

--- app.js
+++ app.js
@@ -3,22 +3,33 @@ const body = document.querySelector('body');
 
 const homePageContent = document.querySelector('.home-page-content')
 
-const projectsNav = document.querySelector('.projects')
+const navList = document.querySelector('ul')
 
 const moon = document.querySelector('.fa-moon')
 
 const sun = document.querySelector('.fa-sun')
 
+const luxehahn = document.querySelector('#luxe-hahn');
+
+const bio = document.querySelector('.bio')
+
 
 // turn on dark mode
 function darkMode() {
     body.classList.add('dark');
+    h1.classList.add('dark');
+    bio.classList.add('dark')
 }
 
 moon.addEventListener('click', darkMode)
 
+
+//turn on light mode
 function lightMode() {
     body.classList.remove('dark');
+    h1.classList.remove('dark')
+    bio.classList.remove('dark')
+
 }
 
 sun.addEventListener('click', lightMode)
@@ -28,7 +39,7 @@ function hideMain() {
     homePageContent.classList.add('hide');
 }
 
-projectsNav.addEventListener('click', hideMain);
+navList.addEventListener('click', hideMain);
 
 
 
